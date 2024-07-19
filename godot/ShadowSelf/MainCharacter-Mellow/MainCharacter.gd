2aafb6df5d02595b8ed350ae36a6c697a9cb16e0
mellow <misccount985@gmail.com>
2024-07-19 22:45:51 +0100

    added walk cycle, minigame pop-up,

--- godot/ShadowSelf/MainCharacter-Mellow/MainCharacter.gd
+++ godot/ShadowSelf/MainCharacter-Mellow/MainCharacter.gd
@@ -1,30 +1,35 @@
-extends CharacterBody2D
+class_name Player extends CharacterBody2D 
 
 @export var speed : int = 60.0
 var playerHasSelectedDirection : bool = false
 var canSelectDirection : bool = true
+var canMove : bool = true
 
 func _input(event):
-	if (event is InputEventMouseButton) and canSelectDirection:
+	if (event is InputEventMouseButton) and canSelectDirection and canMove:
+		$AnimatedSprite2D.play("walk_cycle")
+
 		playerHasSelectedDirection = true
 		self.modulate = Color.AQUA
 		var tween = create_tween()
-		var pos = Vector2(event.position.x, self.position.y)
-		
+		var pos = Vector2(get_global_mouse_position().x, self.position.y)
+
 		if pos.x < self.position.x:
-			tween.tween_property($Sprite2D, "flip_h", true, 1) # flip the sprite
+			tween.tween_property($AnimatedSprite2D, "flip_h", true, 1) # flip the sprite
 		else:
-			tween.tween_property($Sprite2D, "flip_h", false, 1) # dont flip the sprite
+			tween.tween_property($AnimatedSprite2D, "flip_h", false, 1) # dont flip the sprite
 			
 		tween.tween_property(self, "position", pos, 4).set_ease(Tween.EASE_IN_OUT)
 		await tween.finished
 		self.modulate = Color.WHITE
+		$AnimatedSprite2D.play("idle")
 		playerHasSelectedDirection = false
 
 # every 5 seconds, character will start wandering around
 func _on_wander_timeout():
-	if !playerHasSelectedDirection:
-		
+	if !playerHasSelectedDirection and canMove:
+		$AnimatedSprite2D.play("walk_cycle")
+
 		canSelectDirection = false
 		#create tween
 		var tween : Tween= create_tween()
@@ -40,13 +45,14 @@ func _on_wander_timeout():
 
 		match(randomDirection):
 			0: # left
-				tween.tween_property($Sprite2D, "flip_h", true, 0.5) # flip the sprite
+				tween.tween_property($AnimatedSprite2D, "flip_h", true, 0.5) # flip the sprite
 				# move the character in the x axis for a random ammount of pixels. duration is dependent on the distance. Start slow, go faster in the middle, end slow 
 				tween.tween_property(self, "position", Vector2(pos.x - distance, pos.y), 2).set_ease(Tween.EASE_IN_OUT)
 			
 			1: #right
-				tween.tween_property($Sprite2D, "flip_h", false, 0.5)
+				tween.tween_property($AnimatedSprite2D, "flip_h", false, 0.5)
 				tween.tween_property(self, "position", Vector2(pos.x + distance, pos.y), 2).set_ease(Tween.EASE_IN_OUT)
 	
 		await tween.finished
+		$AnimatedSprite2D.play("idle")
 		canSelectDirection = true
