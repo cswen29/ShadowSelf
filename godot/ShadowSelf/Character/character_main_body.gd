cefa38fd8ef2c3bd67e68b0735194a6e3222aa86
Rainworld <alexandersmith1337@hotmail.com>
2024-07-19 16:03:20 -0400

    stuff_added

--- godot/ShadowSelf/Character/character_main_body.gd
+++ godot/ShadowSelf/Character/character_main_body.gd
@@ -2,15 +2,19 @@ extends CharacterBody2D
 
 @onready var floaty = $"../PlayerBodyTest"
 @onready var line = $Line2D
+var distance
 
 func _physics_process(_delta):
 	velocity = Input.get_vector("ui_left","ui_right","ui_up","ui_down")*300
 	move_and_slide()
-	var distance = abs((self.global_position.x - floaty.global_position.x + self.global_position.y - floaty.global_position.y))
+	distance = abs(self.global_position.x - floaty.global_position.x) + abs(self.global_position.y - floaty.global_position.y)
 	if distance >= 800:
 		line.hide()
 	else:
 		line.show()
-	print(line.width)
 	line.width=20
 	line.width=line.width+(-distance/50)
+
+func _input(event):
+	if event.is_action_pressed("leftclick"):
+		print(get_global_mouse_position(),self.global_position,distance)
