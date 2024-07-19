594ef27b65aae353a2110cfbfbfc27eb3ce70883
Rainworld <alexandersmith1337@hotmail.com>
2024-07-19 15:18:29 -0400

    stuff
    idk

--- godot/ShadowSelf/Character/character_main_body.gd
+++ godot/ShadowSelf/Character/character_main_body.gd
@@ -1,7 +1,16 @@
 extends CharacterBody2D
 
-
+@onready var floaty = $"../PlayerBodyTest"
+@onready var line = $Line2D
 
 func _physics_process(_delta):
 	velocity = Input.get_vector("ui_left","ui_right","ui_up","ui_down")*300
 	move_and_slide()
+	var distance = abs((self.global_position.x - floaty.global_position.x + self.global_position.y - floaty.global_position.y))
+	if distance >= 800:
+		line.hide()
+	else:
+		line.show()
+	print(line.width)
+	line.width=20
+	line.width=line.width+(-distance/50)
---
+++ godot/ShadowSelf/CollectableObject/ResourceControl.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+
+func _ready():
+	for child in get_children():
+		pass
+		#child.obj_name.text = child.type
+
+
+func _process(_delta):
+	pass
