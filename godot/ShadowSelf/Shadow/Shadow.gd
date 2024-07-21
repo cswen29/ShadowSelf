e37a88d96f300b9de2ba86a7365cb930554715fe
mellow <misccount985@gmail.com>
2024-07-21 21:04:00 +0100

    Reapply "Merge branch 'main' into mellow"
    This reverts commit 81433292563ffe87612abff410079c144a3f3568.

--- godot/ShadowSelf/Shadow/Shadow.gd
+++ godot/ShadowSelf/Shadow/Shadow.gd
@@ -4,6 +4,4 @@ extends CharacterBody2D
 func _physics_process(_delta):
 	var tween = create_tween()
 	tween.tween_property(self,"position",get_global_mouse_position(),0.1)
-	#self.position = move_toward(self.global_position,get_global_mouse_position(),0.5)
-	#velocity = Input.get_vector("ui_left","ui_right","ui_up","ui_down")*500
 	move_and_slide()
